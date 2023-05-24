328d10e0656de5281b4f0b3940e3f5c9904793b9
Denys Petrenko <denyspetrenko91@gmail.com>
2023-05-24 20:57:59 +0300

    edit index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "./fonts/Roboto-Regular.ttf";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <HashRouter basename = "/react-healthy-food">
+    <HashRouter>
         <App data={data}/>
     </HashRouter>
 );
